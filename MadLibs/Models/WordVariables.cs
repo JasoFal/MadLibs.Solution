ea9e0c00693a88d5f311dc4689166849794cfe75
Jason Falk <jasonfalk9@gmail.com>
2023-10-25 11:42:34 -0700

    Add get set method for the words.

---
+++ MadLibs/Models/WordVariables.cs
@@ -0,0 +1,14 @@
+namespace MadLibs.Models
+{
+  public class WordVariables
+  {
+    public string NounOne { get; set; }
+    public string AdjectiveOne { get; set; }
+    public string VerbOne { get; set; }
+    public string VerbTwo { get; set; }
+    public string AdjectiveTwo { get; set; }
+    public string Adverb { get; set; }
+    public string VerbThree { get; set; }
+    public string NounTwo { get; set; }
+  }
+}
